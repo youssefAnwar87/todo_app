77b4eae8d376412b877f8f1898471e001b6aaa0b
youssefAnwar87 <yassoyosse@gmail.com>
2023-09-27 03:56:52 +0300

    Finished Splash Screen

---
+++ lib/UI/screens/splash/splash_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/UI/screens/home/home_screen.dart';
+import 'package:todo_app/UI/utils/app_assets.dart';
+
+class SplashScreen extends StatefulWidget {
+  static  const String routeName ="splash";
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 2),(){
+      Navigator.pushReplacementNamed(context, HomeScreen.routeName);
+
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Image.asset(AppAssets.splash),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/UI/screens/splash/splash_screen.dart';
+
+import 'UI/screens/home/home_screen.dart';
+import 'UI/utils/app_theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.lightTheme,
+      routes: {
+        SplashScreen.routeName :(_)=> SplashScreen(),
+        HomeScreen.routeName :(_)=> HomeScreen(),
+      },
+      initialRoute: SplashScreen.routeName,
     );
   }
 }
