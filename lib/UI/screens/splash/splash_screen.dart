ae1aa986f9451fc5bb4d806680bda0f453fac2d5
youssefAnwar87 <yassoyosse@gmail.com>
2024-01-29 15:02:12 +0200

    added dark splash

--- lib/UI/screens/splash/splash_screen.dart
+++ lib/UI/screens/splash/splash_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todo_app/UI/screens/home/home_screen.dart';
 import 'package:todo_app/UI/screens/login/login.dart';
 import 'package:todo_app/UI/screens/register/register.dart';
+import 'package:todo_app/UI/settingsProvider/settings_provider.dart';
 import 'package:todo_app/UI/utils/app_assets.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -22,8 +24,10 @@ class _SplashScreenState extends State<SplashScreen> {
   }
   @override
   Widget build(BuildContext context) {
+    SettingProvider sprovider = Provider.of(context);
+
     return Scaffold(
-      body: Image.asset(AppAssets.splash),
+      body: sprovider.isDark() ? Image.asset(AppAssets.Darksplash) : Image.asset(AppAssets.splash),
     );
   }
 }
--- lib/UI/utils/app_assets.dart
+++ lib/UI/utils/app_assets.dart
@@ -1,5 +1,6 @@
 abstract class  AppAssets{
   static const String imagePath ="assets/images/";
   static const String splash ="${imagePath}splash.png";
+  static const String Darksplash ="${imagePath}splashDark.png";
   static const String login ="${imagePath}SIGN IN – 1.png";
 }
